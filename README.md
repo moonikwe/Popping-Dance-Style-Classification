bd6970f7c4b0f797597bc03e55e14aecc6fac9c7
faielomente <moomeguillo@gmail.com>
2018-06-01 14:55:59 +0800

    reinitialized

---
+++ README.md
@@ -0,0 +1,41 @@
+# Popping Dance Style Classification Through Inception V3 Feature Extraction and LSTM Classification
+
+This program classifies Popping Dance Styles through using Inception V3 for feature extraction and LSTM for classification, as proposed by Harvey (2017) in his blog, Five Video Classification Methods.
+This metholodogy was applied to my own dataset composed of 1310 Popping Dance style videos under 14 classes. 
+
+See this link for the full details of the said blog https://medium.com/@harvitronix/five-video-classification-methods-implemented-in-keras-and-tensorflow-99cad29cc0b5
+
+The whole methodology for method #4 (as adopted in this project) is seen in the same link. Refer to this for his github repository Popping-Dance-Style-Classification
+
+##Requirements
+
+Keras>=2.0.2
+numpy>=1.12.1
+pandas>=0.19.2
+tqdm>=4.11.2
+matplotlib>=2.0.0
+Pillow>=2.1.0
+h5py>=2.7.0
+
+see requirements.py
+
+ffmpeg <https://www.ffmpeg.org/>
+VideoPad <http://www.nchsoftware.com/videopad/index.html>
+
+##How to Install
+
+##How to Use
+
+##Results
+
+##References
+
+##License
+
+
+
+## Methodology Citation
+
+Harvey, M. (2017). Five video classification methods implemented in Keras and TensorFlow [Blog Post]. Retrieved from https://blog.coast.ai/five-video-classification -methods-implemented-in-keras-and-tensorflow-99cad29cc0b5
+
+
---
+++ models.py
@@ -0,0 +1,60 @@
+"""
+A collection of models we'll use to attempt to classify videos.
+"""
+from keras.layers import Dense, Flatten, Dropout, ZeroPadding3D
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential, load_model
+from keras.optimizers import Adam, RMSprop, SGD
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.convolutional import (Conv2D, MaxPooling3D, Conv3D,
+    MaxPooling2D)
+from collections import deque
+import sys
+
+class ResearchModels():
+    def __init__(self, nb_classes, model, seq_length,
+                 saved_model=None, features_length=2048):
+
+        # Set defaults.
+        self.seq_length = seq_length
+        self.load_model = load_model
+        self.saved_model = saved_model
+        self.nb_classes = nb_classes
+        self.feature_queue = deque()
+
+        # Set the metrics. Only use top k if there's a need.
+        metrics = ['accuracy']
+        if self.nb_classes >= 10:
+            metrics.append('top_k_categorical_accuracy')
+
+        # Get the appropriate model.
+        if self.saved_model is not None:
+            print("Loading model %s" % self.saved_model)
+            self.model = load_model(self.saved_model)
+        elif model == 'lstm':
+            print("Loading LSTM model.")
+            self.input_shape = (seq_length, features_length)
+            self.model = self.lstm()
+        else:
+            print("Unknown network.")
+            sys.exit()
+
+        # Now compile the network.
+        optimizer = Adam(lr=1e-5, decay=1e-6)
+ 
+        self.model.compile(loss='binary_crossentropy', optimizer=optimizer,
+                           metrics=metrics)
+
+        print(self.model.summary())
+
+    def lstm(self):
+        model = Sequential()
+        model.add(LSTM(2048, return_sequences=False,
+                       input_shape=(804, 2048),
+                       dropout=0.5))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.nb_classes, activation='softmax'))
+
+        return model
+
